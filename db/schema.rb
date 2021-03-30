fe2a59f5b3bf14296e9f3c79cf94e570e3963cb9
ismaelvr <ismaelvr27@gmail.com>
2021-03-30 20:12:08 +0200

    Version 1.0.8. Dark mode and images in notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_163530) do
+ActiveRecord::Schema.define(version: 2021_03_30_171403) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.binary "img"
   end
 
 end
